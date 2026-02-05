baef7f18c15d11dbbd088ca6017da44576d29d6c
Jorge Diez <jorgealejandrodiez@gmail.com>
2026-02-05 10:51:24 -0600

    Configure GitHub Pages deployment and branching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/GCDD-Web-Redesign',
+  assetPrefix: '/GCDD-Web-Redesign',
 };
 
 export default nextConfig;
